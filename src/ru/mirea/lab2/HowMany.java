c2bb0592f0ec5301e6f8d4d77f4c054d2fed771b
lexus14 <lexus14adm@bk.ru>
2023-09-30 01:17:01 +0300

    a

--- src/ru/mirea/lab2/HowMany.java
+++ src/ru/mirea/lab2/HowMany.java
@@ -1,2 +1,21 @@
-package ru.mirea.lab2;public class HowMany {
+package ru.mirea.lab2;
+
+import java.util.Scanner;
+public class HowMany {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        String input = a.nextLine();
+        if(input.length() == 0) {
+            System.out.println("Хоть что-то бы ввёл для приличия((");
+            return;
+        }
+        int b = 1;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == ' ') {
+                b++;
+            }
+        }
+        System.out.println(b);
+    }
 }
+
--- src/ru/mirea/lab2/_8.java
+++ src/ru/mirea/lab2/_8.java
@@ -1,2 +1,23 @@
-package ru.mirea.lab2;public class _8 {
+package ru.mirea.lab2;
+
+import java.util.Scanner;
+public class _8 {
+
+    public static void main(String[] args){
+        String[] a = {"Пример ","массива ","строчного ","типа "};
+        for (int i=0; i<a.length; i++){
+            System.out.print(a[i]);
+        }
+        System.out.println();
+        int b = a.length;
+        String temp;
+        for (int i = 0; i < b/2; i++) {
+            temp = a[b-i-1];
+            a[b-i-1] = a[i];
+            a[i] = temp;
+        }
+        for (int i=0; i<b; i++){
+            System.out.print(a[i]);
+        }
+    }
 }
