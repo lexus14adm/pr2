47564be55dfa634108028eb274cf21638630c40f
lexus14 <lexus14adm@bk.ru>
2023-10-19 21:35:32 +0300

    a

--- src/ru/mirea/lab3/convertor.java
+++ src/ru/mirea/lab3/convertor.java
@@ -2,23 +2,38 @@ package ru.mirea.lab3;
 
 import java.util.Scanner;
 public class convertor {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
         double dol = 0.0101;
         double eur = 0.0107;
-        System.out.println("Перевести в: 1 - доллар, 2 - евро");
-        Integer b = a.nextInt();
-        if ((b != 1) && (b != 2)){
-            System.out.println("Ошибка: веверный ввод данных");
-            return;
+        int i = 0;
+        Integer b = null;
+        while (i == 0) {
+            System.out.println("Перевести в: 1 - доллар, 2 - евро");
+            b = a.nextInt();
+            if ((b != 1) && (b != 2)) {
+                System.out.println("Ошибка: веверный ввод данных");
+            }
+            else {
+                i = 1;
+            }
         }
-        System.out.println("Рублей: ");
-        Double c = a.nextDouble();
+        Double c = null;
+        while (i == 1) {
+            System.out.println("Рублей: ");
+            c = a.nextDouble();
+            if (c < 0) {
+                System.out.println("Введите значение больше нуля");
+            }
+            else{
+                i = 0;
+            }
+        }
         if (b == 1) {
-            System.out.println(c*dol);
+            System.out.println(c * dol);
         }
         if (b == 2) {
-            System.out.println(c*eur);
+            System.out.println(c * eur);
         }
     }
 }
---
+++ src/ru/mirea/lab4_1/Mat.java
@@ -0,0 +1,47 @@
+package ru.mirea.lab4_1;
+
+import java.util.Arrays;
+public class Mat{
+    static int str = 2;
+    static int sto = 2;
+    static int[][] m = new int [str][sto];
+
+    public static void set(){
+        m[0][0]  = 0;
+        m[0][1] = 0;
+        m[1][0] = 0;
+        m[1][1] = 0;
+    }
+
+    public static void get(){
+        System.out.println(Arrays.deepToString(m));
+    }
+
+    public static void sum(int[][] n){
+        if (n[0].length + n.length != str + sto){
+            System.out.println("Неправильно введёная матрица");
+            return;
+        }
+        for (int i = 0; i < 2; i++){
+            for (int j = 0; j < 2; j++){
+                m[i][j] += n[i][j];
+            }
+        }
+    }
+    public static void mul(int g){
+        for (int i = 0; i < 2; i++){
+            for (int j = 0; j < 2; j++){
+                m[i][j] *= g;
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        set();
+        get();
+        int[][] n = {{3,2},{1, 0}};
+        sum(n);
+        mul(-4);
+        get();
+    }
+}
